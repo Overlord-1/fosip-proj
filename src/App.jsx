ca5bb37d690ebf345a54452d0ec662675d608fdf
Ruchir <ruchirkhare12@gmail.com>
2024-11-02 00:01:08 +0530

    init 2
    ::::qa
    new file:   index.html

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen font-bold bg-gray-800 text-green-300 text-6xl">
+      <h1>FOSIP project setup</h1>
+    </div>
+  )
+}
+
+export default App
